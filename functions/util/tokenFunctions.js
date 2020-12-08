3926f67136116bd99a629dc5559872c690fe00a5
Mark <mrk.rosenberg@gmail.com>
2020-12-08 10:43:08 -0700

    comments out unused functions

--- functions/util/tokenFunctions.js
+++ functions/util/tokenFunctions.js
@@ -13,17 +13,6 @@ exports.generateToken = (config) => {
 };
 
 // Token functions
-exports.chatToken = (identity, config) => {
-
-    const chatGrant = new ChatGrant({
-        serviceSid: config.twilio.chatService
-    });
-    const token = generateToken(config);
-    token.addGrant(chatGrant);
-    token.identity = identity;
-    return token;
-};
-
 exports.videoToken = (identity, room, config) => {
     
     let videoGrant;
@@ -38,21 +27,34 @@ exports.videoToken = (identity, room, config) => {
     return token;
 };
 
-exports.voiceToken = (identity, config) => {
+// exports.chatToken = (identity, config) => {
 
-    let voiceGrant;
-    if (typeof config.twilio.outgoingApplicationSid !== 'undefined') {
-        voiceGrant = new VoiceGrant({
-            outgoingApplicationSid: config.twilio.outgoingApplicationSid,
-            incomingAllow: config.twilio.incomingAllow
-        });
-    } else {
-        voiceGrant = new VoiceGrant({
-            incomingAllow: config.twilio.incomingAllow
-        })
-    };
-    const token = generateToken(config);
-    token.addGrant(voiceGrant);
-    token.identity = identity;
-    return token;
-};
+//     const chatGrant = new ChatGrant({
+//         serviceSid: config.twilio.chatService
+//     });
+//     const token = generateToken(config);
+//     token.addGrant(chatGrant);
+//     token.identity = identity;
+//     return token;
+// };
+
+
+
+// exports.voiceToken = (identity, config) => {
+
+//     let voiceGrant;
+//     if (typeof config.twilio.outgoingApplicationSid !== 'undefined') {
+//         voiceGrant = new VoiceGrant({
+//             outgoingApplicationSid: config.twilio.outgoingApplicationSid,
+//             incomingAllow: config.twilio.incomingAllow
+//         });
+//     } else {
+//         voiceGrant = new VoiceGrant({
+//             incomingAllow: config.twilio.incomingAllow
+//         })
+//     };
+//     const token = generateToken(config);
+//     token.addGrant(voiceGrant);
+//     token.identity = identity;
+//     return token;
+// };
